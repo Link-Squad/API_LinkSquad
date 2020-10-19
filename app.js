5d1ca3fca5dec43b0855095486e02ce743a49f5a
TL Garcia <tomas.bl.garcia@gmail.com>
2020-10-19 20:01:03 +0200

    chore: Move errorHandler to middlewares

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const passportConfig = require('./config/passport.config');
 const session = require('./config/session.config');
 const cors = require('./config/cors.config');
 const {normalizePort} = require('./helpers/helpers');
+const httpErrors = require('./middlewares/httpErrors.middleware');
 
-/* CONFIG EXPRESS */
+/* MIDDLEWARE */
 const app = express();
 //app.use(cors);
 app.use(logger('dev'));
@@ -20,44 +21,21 @@ app.use(logger('dev'));
 //app.use(session);
 //app.use(passportConfig);
 
-/* Configure routes */
+
+/* ROUTES */
 const router = require('./config/routes.js');
 app.use('/', router);
 
-// catch 404 and forward to error handler
+/*ERRORS*/
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (error, req, res, next) {
-  console.error('-' * 1000);
-  console.error(error);
-
-  res.status(error.status || 500);
-
-  const data = {};
-
-  if (error instanceof mongoose.Error.ValidationError) {
-    res.status(400);
-
-    for (let field of Object.keys(error.errors)) {
-      error.errors[field] = error.errors[field].message;
-    }
-
-    data.errors = error.errors;
-  } else if (error instanceof mongoose.Error.CastError) {
-    error = createError(404, 'Resource not found');
-  }
-
-  data.message = error.message;
-  res.json(data);
-});
+app.use(httpErrors.errorHandler);
 
 /* Listen on provided port */
 const port = normalizePort(process.env.PORT);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-// Helper functions
---
+++ middlewares/httpErrors.middleware.js
@@ -0,0 +1,25 @@
+//const app = require('express');
+
+module.exports.errorHandler = (error, req, res, next) => {
+  console.error('-' * 1000);
+  console.error(error);
+
+  res.status(error.status || 500);
+
+  const data = {};
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    res.status(400);
+
+    for (let field of Object.keys(error.errors)) {
+      error.errors[field] = error.errors[field].message;
+    }
+
+    data.errors = error.errors;
+  } else if (error instanceof mongoose.Error.CastError) {
+    error = createError(404, 'Resource not found');
+  }
+
+  data.message = error.message;
+  res.json(data);
+};
