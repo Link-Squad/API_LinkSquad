25e5789b4d6576f553d54529e98f074cbef452d5
Pumpel <46710520+Pumpelgit@users.noreply.github.com>
2020-10-20 11:33:57 +0200

    feat: Create Games model & Games end point
    Added basic games model. Also added a default image to use for default cases.

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const usersController = require('../controllers/users.controller');
-
+const usersController = require("../controllers/users.controller");
+const gamesController = require("../controllers/games.controller");
 module.exports = router;
 
-router.get('/', (req, res, send) => {
-    res.send('Hello you');
+router.get("/", (req, res, send) => {
+  res.send("Hello you");
 });
 
 /* USERS */
-router.post('/login', usersController.doLogin);
-router.post('/users', usersController.createUser);
+router.post("/login", usersController.doLogin);
+router.post("/users", usersController.createUser);
+
+/* GAMES */
+router.get("/games", gamesController.getGames);
