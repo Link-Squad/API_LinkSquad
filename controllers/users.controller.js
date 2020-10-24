746f39326905e52ad01c351b308acd8b21ac2dad
TL Garcia <tomas.bl.garcia@gmail.com>
2020-10-24 12:27:39 +0200

    minor

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -45,7 +45,6 @@ module.exports.createUser = (req, res, next) => {
 
 module.exports.listUsers = (req, res, next) => {
 	const query = req.query || {};
-	console.log(query)
 
 	User.find(query)
 		.then((users) => {
