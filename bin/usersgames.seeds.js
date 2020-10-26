463f6288e3b537bec9019d427c013a8957fb2746
Pumpel <romanbaos@gmail.com>
2020-10-26 20:11:20 +0100

    feat: users games seeds

---
+++ bin/usersgames.seeds.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+require('../config/db.config');
+
+const {
+  returnRandomSubArray,
+  returnRandomNumer
+} = require('../helpers/helpers');
+
+const UserGame = require('../models/userGame.model');
+
+const createUsersGames = (users, games, maxAmountPerUser) => {
+  const usersGames = [];
+  for (let i = 0; i < users.length; i++) {
+    const userGames = returnRandomSubArray(games, returnRandomNumer(1,maxAmountPerUser));
+    userGames.forEach((el) => {
+      const newUserGame = {
+        game: el,
+        user: users[i],
+      };
+      usersGames.push(newUserGame);
+    });
+  }
+  return UserGame.create(usersGames);
+};
+
+module.exports = createUsersGames;
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,42 +1,48 @@
 module.exports.normalizePort = (val) => {
-	const port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-	if (isNaN(port)) {
-		return val;
-	}
+  if (isNaN(port)) {
+    return val;
+  }
 
-	if (port >= 0) {
-		return port;
-	}
+  if (port >= 0) {
+    return port;
+  }
 
-	return false;
+  return false;
 };
 
 module.exports.generateRandomToken = () => {
-	const characters =
-		'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	let token = '';
-	for (let i = 0; i < 25; i++) {
-		token += characters[Math.floor(Math.random() * characters.length)];
-	}
-	return token;
+  const characters =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 0; i < 25; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return token;
 };
 
 module.exports.returnRandomSubArray = (array, amount) => {
-	const randElements = [];
-
-	for (let i = 0; i < amount; i++) {
-		const randIndex = Math.floor(Math.random() * array.length);
-		const randElement = array[randIndex];
-
-		randElements.push(randElement);
-	}
-	return randElements;
+  const randElements = [];
+  const maxIndex = amount >= array.length ? array.length : amount;
+  for (let i = 0; i < maxIndex; i++) {
+    const randIndex = Math.floor(Math.random() * array.length);
+    const randElement = array[randIndex];
+    randElements.push(randElement);
+  }
+  randElements.filter((el, i) => randElements.indexOf(el) === i);
+  return randElements;
 };
 
 module.exports.returnRandomElement = (array) => {
-		const randIndex = Math.floor(Math.random() * array.length);
-		const randElement = array[randIndex];
+  const randIndex = Math.floor(Math.random() * array.length);
+  const randElement = array[randIndex];
 
-		return randElement;
+  return randElement;
+};
+
+module.exports.returnRandomNumer = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 };
