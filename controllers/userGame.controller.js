f44e17ffa74bc6c174f9b81e2da3d2be517dcbd2
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-06 19:37:15 +0100

    feat: Get Users by Game ID

--- controllers/userGame.controller.js
+++ controllers/userGame.controller.js
@@ -1,27 +1,38 @@
 const mongoose = require('mongoose');
+const User = require('../models/user.model');
 const UserGame = require('../models/userGame.model');
 
 module.exports.getUserGame = (req, res, next) => {
-  const query = req.query;
-  UserGame.find(query)
-    .then((results) => res.status(201).json(results))
-    .catch((error) => next(createError(400, error)));
+	const query = req.query;
+	UserGame.find(query)
+		.then(results => res.status(201).json(results))
+		.catch(error => next(createError(400, error)));
+};
+
+module.exports.getUsersByGame = (req, res, next) => {
+	const { gameId } = req.params;
+	UserGame.find({ game: gameId })
+		.populate('user')
+		.then(userGames => {
+      const userArray = userGames.map(uG => uG.user);
+      res.status(200).json(userArray)
+		});
 };
 
 module.exports.createUserGame = (req, res, next) => {
-  const { game, user } = req.query;
+	const { game, user } = req.query;
 
-  const newUserGame = new UserGame({ game, user });
-  newUserGame
-    .save()
-    .then((result) => res.status(201).json(result))
-    .catch((error) => next(createError(400, error)));
+	const newUserGame = new UserGame({ game, user });
+	newUserGame
+		.save()
+		.then(result => res.status(201).json(result))
+		.catch(error => next(createError(400, error)));
 };
 
 module.exports.deleteUserGame = (req, res, next) => {
-  const query = req.query;
+	const query = req.query;
 
-  UserGame.deleteMany(query)
-    .then(() => res.status(201).json({ response: 'ok' }))
-    .catch((error) => next(createError(400, error)));
+	UserGame.deleteMany(query)
+		.then(() => res.status(201).json({ response: 'ok' }))
+		.catch(error => next(createError(400, error)));
 };
