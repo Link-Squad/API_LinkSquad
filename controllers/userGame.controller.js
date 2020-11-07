60792a38a4afc19cc02e111d81fe3a1af8a4cf82
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-07 01:11:11 +0100

    checkpoint

--- controllers/userGame.controller.js
+++ controllers/userGame.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user.model');
 const UserGame = require('../models/userGame.model');
+const createError = require('http-errors');
 
 module.exports.getUserGame = (req, res, next) => {
 	const query = req.query;
@@ -14,13 +15,14 @@ module.exports.getUsersByGame = (req, res, next) => {
 	UserGame.find({ game: gameId })
 		.populate('user')
 		.then(userGames => {
-      const userArray = userGames.map(uG => uG.user);
-      res.status(200).json(userArray)
+			const userArray = userGames.map(uG => uG.user);
+			res.status(200).json(userArray);
 		});
 };
 
 module.exports.createUserGame = (req, res, next) => {
-	const { game, user } = req.query;
+  const { game } = req.body;
+  const user = req.session.user.id;
 
 	const newUserGame = new UserGame({ game, user });
 	newUserGame
