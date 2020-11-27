a84d4df1206a3128f708c357c5aadc04adcda4e5
TL-Garcia <tomas.bl.garcia@gmail.com>
2020-11-27 15:08:44 +0100

    chore: Set Mongoose useFindAndModify to false

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const DB_URI = process.env.DB_URI || 'mongodb://127.0.0.1:27017';
 
 mongoose
-	.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+	.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false})
 	.then(() => console.info(`Established connection to the database on ${DB_URI}`))
 	.catch(error => console.error('Database connection unsuccesful', error));
 
